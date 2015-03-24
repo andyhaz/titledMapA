df934e7fcd9db689bdbd132be975be2fbb9d22d7
andyhaz <andyhaz1@AndyAir.local>
2015-03-24 15:50:27 -0700

    full working version. but still problem with copying files

--- TiledMapA/GameScene.m
+++ TiledMapA/GameScene.m
@@ -8,31 +8,57 @@
 
 #import "GameScene.h"
 #import "JSTileMap.h"
-
-//#import "game_utils.h"
-//#import "levelMap.h"
+#import "game_utils.h"
+#import "levelMap.h"
 
 @implementation GameScene {
-    SKSpriteNode *map;
+    SKSpriteNode *sprite,*circleTraget;
+    levelMap *mapLevelNode;
 }
+
+typedef enum : uint8_t {
+    playerCategory     = 1,
+    wallCategory       = 2,
+    circleCategory     = 3,
+} APAColliderType;
 
 -(void)didMoveToView:(SKView *)view {
     NSLog(@"screen size:%f %f",self.frame.size.width,self.frame.size.height);
     /* Setup your scene here */
-    
+    self.physicsWorld.gravity = CGVectorMake(0,0);
+    self.physicsWorld.contactDelegate = self;
    //load map
-     JSTileMap *tileMap = [JSTileMap mapNamed:@"map1.tmx"];
+  /*   JSTileMap *tileMap = [JSTileMap mapNamed:@"map1.tmx"];
     [self addChild:tileMap];
+    tileMap.xScale = 1.0;
+    tileMap.yScale = 1,0;*/
+    mapLevelNode = [[levelMap alloc]init];
+    [mapLevelNode loadMap:@"map1.tmx"];
+    mapLevelNode.name = @"levelMap";
+    mapLevelNode.xScale = 1.0;
+    mapLevelNode.yScale = 1.0;
+   [self addChild:mapLevelNode];
     
+    //add target
+    circleTraget = [SKSpriteNode spriteNodeWithImageNamed:@"cirImage.png"];
+    circleTraget.xScale = 0.5;
+    circleTraget.yScale = 0.5;
+    circleTraget.position = CGPointMake(CGRectGetMidX(self.frame),
+                                       CGRectGetMidY(self.frame));
+    [self addChild:circleTraget];
     
-    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+    sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
     
-    myLabel.text = @"Hello, World!";
-    myLabel.fontSize = 65;
-    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                   CGRectGetMidY(self.frame));
+    sprite.xScale = 0.2;
+    sprite.yScale = 0.2;
+    sprite.position = CGPointMake(CGRectGetMidX(self.frame),
+                                  CGRectGetMidY(self.frame));
+    sprite.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:sprite.frame.size.width / 3.0f];
+    sprite.physicsBody.dynamic = YES;
+    sprite.physicsBody.categoryBitMask = playerCategory;
+    sprite.physicsBody.contactTestBitMask = wallCategory | circleCategory;
     
-    [self addChild:myLabel];
+    [self addChild:sprite];
     
 }
 
@@ -41,18 +67,9 @@
     
     for (UITouch *touch in touches) {
         CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.xScale = 0.5;
-        sprite.yScale = 0.5;
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
+        circleTraget.position = location;
+        [sprite runAction:[self rotImage]];
+        [sprite runAction:[self moveAction:location.x :location.y :@"target" :0.0020]];
     }
 }
 
@@ -60,4 +77,56 @@
     /* Called before each frame is rendered */
 }
 
+-(SKAction*)rotImage{
+    game_utils *gu  = [[game_utils alloc]init];
+    CGPoint p1 = CGPointMake(sprite.position.x,sprite.position.y);
+    CGPoint p2 = CGPointMake(circleTraget.position.x,circleTraget.position.y);
+    
+    float angle = [gu pointPairToBearingDegrees:p1 secondPoint:p2];
+    SKAction *rotPuffer = [SKAction rotateToAngle:-angle duration:0.2];
+    return rotPuffer;
+}//end rotimage
+
+-(SKAction*)moveAction :(float)x :(float)y :(NSString*)sprintName :(float)speed{
+    //the node you want to move
+    SKNode *node = [self childNodeWithName:sprintName];
+    //   NSLog(@"moveAction:%@",node);
+    
+    //get the distance between the destination position and the node's position
+    double distance = sqrt(pow((x - node.position.x), 2.0) + pow((y - node.position.y), 2.0));
+    
+    //calculate your new duration based on the distance
+    float moveDuration = speed*distance;
+    
+    //move the node
+    SKAction *move = [SKAction moveTo:CGPointMake(x,y) duration: moveDuration];
+    
+    return move;
+}//end move action
+//
+- (void)didBeginContact:(SKPhysicsContact *)contact {
+    SKPhysicsBody *firstBody, *secondBody;
+    //  NSString *bodyStr = contact.bodyB.node.name;
+    //  NSString *bodyStrA = contact.bodyA.node.name;
+    // NSLog(@" bodyStr:%@ boodStrA:%@",bodyStr,bodyStrA);
+    if (contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask)
+    {
+        firstBody = contact.bodyA;
+        secondBody = contact.bodyB;
+    } else {
+        firstBody = contact.bodyB;
+        secondBody = contact.bodyA;
+    }
+    //walls
+    if (firstBody.categoryBitMask == playerCategory  && secondBody.categoryBitMask == wallCategory) {
+        //NSLog(@"wall ");
+        [sprite removeAllActions];
+    }
+    
+    if (firstBody.categoryBitMask == playerCategory  && secondBody.categoryBitMask == circleCategory) {
+        // NSLog(@"target bodyStr:%@ boodStrA:%@",bodyStr,bodyStrA);
+        //  [tileMap removeAllActions];
+      //  circleTraget.alpha = 0.0;
+    }
+}
 @end
